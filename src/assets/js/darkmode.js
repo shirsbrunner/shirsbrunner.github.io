e914b0b7d31aa73a54882d03f5acc43d560ef1d1
shirsbrunner <simon.a.hirsbrunner@gmail.com>
2022-02-27 21:12:58 +0100

    Update to darkmode
    darkmode preference is now stored
    styling toggle
    vertical centering bot nav

--- src/assets/js/darkmode.js
+++ src/assets/js/darkmode.js
@@ -1,22 +1,36 @@
 const DM_BTN = document.querySelector(".dmbtntoggler");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const currentTheme = localStorage.getItem("theme"); //checks for previously saved darkmode
 
-//console.log(DM_BTN);
-//console.log(prefersDarkScheme);
+if (currentTheme == "dark"){
+  //toggle the dark theme
+  document.documentElement.classList.toggle("dark");
+} else if (currentTheme == "light") {
+  //toggle light
+  document.documentElement.classList.toggle("light");
+}
 
 // Listen for a click on the button 
 DM_BTN.addEventListener("click", function() {
-    //alert("Behold the button is working!!");
+
     // If the OS is set to dark mode...
     if (prefersDarkScheme.matches) {
       // ...then apply the .light-theme class to override those styles
       //put media to light-theme? 
-      console.log("now switching to light")
+      console.log("now switching to light?")
       document.documentElement.classList.toggle("light");
+      //but use dark-mode if light mode is already set
+      var theme = document.documentElement.classList.contains("light") ? "light" : "dark";
+      console.log(theme);
       // Otherwise...
+
     } else {
       // ...apply the .dark-theme class to override the default light styles
-      console.log("now switching to dark")
+      console.log("now switching to dark?")
       document.documentElement.classList.toggle("dark");
+      var theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+      console.log(theme);
     }
+    //save the current seting
+    localStorage.setItem("theme", theme);
   });
