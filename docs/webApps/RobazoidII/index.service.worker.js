b9616b97b1df6b8d7fbcac39bc2ca1b4fa54ef86
Simon Hirsbrunner <simon.a.hirsbrunner@gmail.com>
2026-03-12 20:53:50 +0100

    touch trigger

--- docs/webApps/RobazoidII/index.service.worker.js
+++ docs/webApps/RobazoidII/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773343915|1871231495';
+const CACHE_VERSION = '1773345206|460412523';
 /** @type {string} */
 const CACHE_PREFIX = 'Robazoid_II-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
