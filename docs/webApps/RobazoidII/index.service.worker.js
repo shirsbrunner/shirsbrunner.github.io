ede7d859e6e6c5743133abda588e8579bc4553a6
Simon Hirsbrunner <simon.a.hirsbrunner@gmail.com>
2026-03-14 20:57:49 +0100

    roba next

--- docs/webApps/RobazoidII/index.service.worker.js
+++ docs/webApps/RobazoidII/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773420739|2821707816';
+const CACHE_VERSION = '1773518123|2397256161';
 /** @type {string} */
 const CACHE_PREFIX = 'Robazoid_II-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
